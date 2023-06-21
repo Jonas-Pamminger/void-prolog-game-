3f3c72436d0fdc3db22d6b9cd63734b45f32a0c3
PeaceMaker <85544073+FabianBaitura@users.noreply.github.com>
2023-06-21 21:39:24 +0200

    adding story and ending

--- Game/game.pl
+++ Game/game.pl
@@ -179,7 +179,9 @@ start :-
    circumstances, a room may have more than one description. */
 
 describe(droneDock1) :-
-    write('You are in Drone Dock 1. The air is filled with the hum of machinery.'), nl.
+    write('You are in Drone Dock 1. You seem to have fallen asleep at your desk, something seems to be off tho. you''ve awoken with a slight headache. Your place is a mess.'),
+    write('The drone you were fixing seems to just lie there on the ground, with all of it previously neatly placed technical parts scattered everywhere. What a piece of work.'),
+    write('You decide to slowly stand up, even with that headache of yours, you should get to the bridge and inform yourself of what happend.'), nl.
 
 describe(hall1) :-
     write('You are in a narrow hallway. There is a fuse case on the wall.'), nl.
@@ -191,31 +193,38 @@ describe(hall3) :-
     write('You are in a long corridor. The walls are lined with blinking lights.'), nl.
 
 describe(mainDock) :-
-    write('You enter the main dock. Large spacecraft are being loaded with cargo.'), nl.
+    write('You enter the main dock. It is.. empty? No one seems to be loading up any ships, on secound thought, all of the ships seem to have... departed?'), nl.
 
 describe(generator) :-
-    write('You arrive at the generator room. It hums with energy.'), nl.
+    write('You arrive at the generator room. It hums with energy. No engineers seem to be around tho. Just you and the hum.'), nl.
 
 describe(dinningRoom) :-
     write('You step into the dining room. Tables are set for a meal, but no one is here.'), nl.
 
 describe(kitchen) :-
-    write('You enter the kitchen. The smell of freshly baked bread fills the air.'), nl.
+    write('You enter the kitchen. Food Items are scattered on the ground, nothing of use tho, chef seems to be missing. Weird, he usually never leaves his post.'), nl.
 
 describe(livingQuarters) :-
-    write('You step into the living quarters. The rooms are small but cozy.'), nl.
+    write('You step into the living quarters. The rooms are small but cozy. On the wall is a poster, containing a chibby cat in an austronaut suit. Under it is a motto: "KEEP CALM".'),
+    write('You find a picture containing you and the rest of the crew, you remember the smiles they had on they''re faces when they were first introduced to they''re new vessel. But'),
+    write('that was years ago. still, you remember the day the crew was assembled fondly, and you''ll never forget the family you''ve forged along the way.'), nl.
 
 describe(lockerRoom) :-
-    write('You are in the locker room. Rows of lockers line the walls.'), nl.
+    write('You are in the locker room. Rows of lockers line the walls. The spacesuits usually laying in there which haven''t been used for ages are all gone.'),
+    write('Only yours remains. You do remember tho that it''s broken. You remember the day you went out to fix a sattelite and your front shield cracked, you were lucky the'),
+    write('safety drones cought wind of your situation and dragged you back into the airlock. Your suit hasn''t been repaired to this day.'), nl.
 
 describe(conferenceRoom) :-
-    write('You enter the conference room. A large round table dominates the center.'), nl.
+    write('You enter the conference room. A large round table dominates the center. Yet no one is currently having a seat.'), nl.
 
 describe(mainBridge) :-
-    write('You finally arrive at the main bridge. It is the command center of the ship.'), nl.
+    write('You finally arrive at the main bridge, but no one is here. It is quite dark, there are shards of glass covering the floor. You guess the lights were knocked out, how, you have no Idea.'),
+    write('There is one thing only which lights up the bridge, the big screen in the middle, displaying the current situation all over the ship. You take a look at whats going on and horror starts to arise'),
+    write('"SHIP OXYGEN LEVEL: 3% WARNING, ALL CREW ON BOARD IMMEDIATLY PUT ON YOUR SUITS, SHIP IN CRITICAL CONDITION, LEAVE THE SHIP BY ANY MEANS NECESSARY"'),
+    write('You look at the ships integrity system and realize that half of the ship is utterly destroyed. You start connecting the dots, and then it hits you. All available space shuttles have long departed...'), die, nl.
 
 describe(storageRoom) :-
     write('You are in a storage room. Various items are stacked on shelves.'), nl.
 
     describe(droneDock2):-
-    write('You are in DroneDock2'), nl.
+    write('You are in Drone Dock 2'), nl.
